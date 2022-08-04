5ad81f936fd414c567b61d1c87b57e7590037ed7
Burak Metehan Tunçel <burakmetehan.123@hotmail.com>
2022-08-04 09:16:52 +0300

    Initial commit

--- src/main/java/tr/com/obss/jip/springfinal/JipSpringFinalProjectApplication.java
+++ src/main/java/tr/com/obss/jip/springfinal/JipSpringFinalProjectApplication.java
@@ -2,8 +2,16 @@ package tr.com.obss.jip.springfinal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        HibernateJpaAutoConfiguration.class,
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        SecurityAutoConfiguration.class})
 public class JipSpringFinalProjectApplication {
 
     public static void main(String[] args) {
