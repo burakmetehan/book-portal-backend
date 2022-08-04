b4d5a233d5795ce79103a23b5649905d05204709
Burak Metehan Tunçel <burakmetehan.123@hotmail.com>
2022-08-04 09:10:15 +0300

    Initial commit

---
+++ src/main/java/tr/com/obss/jip/springfinal/JipSpringFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package tr.com.obss.jip.springfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JipSpringFinalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JipSpringFinalProjectApplication.class, args);
+    }
+
+}
