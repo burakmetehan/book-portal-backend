15b191c881761de6d0391e8d160ea9b9c293e1a1
Burak Metehan Tunçel <burakmetehan.123@hotmail.com>
2022-08-23 23:58:55 +0300

    Update WebSecurityConfig

--- src/main/java/tr/com/obss/jip/springfinal/config/WebSecurityConfig.java
+++ src/main/java/tr/com/obss/jip/springfinal/config/WebSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -51,20 +52,25 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        // Disable CSRF
+        // Enable CORS and Disable CSRF
         http = http.cors().and().csrf().disable();
 
         // Set session management to stateless
-        //http = http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and();
+        http = http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and();
 
         // Set unauthorized requests exception handler
         /*http = http.exceptionHandling().authenticationEntryPoint((request, response, ex) -> {
             response.sendError(SC_UNAUTHORIZED, ex.getMessage());
         }).and();*/
 
-        http = http.authorizeHttpRequests().antMatchers("/authenticate").permitAll().anyRequest()
-                   //.hasAnyAuthority("ROLE_ADMIN", "ROLE_USER")
-                   .permitAll().and();
+        http = http.authorizeHttpRequests()
+                   .antMatchers("/authenticate")
+                   .permitAll()
+                   .antMatchers("/login")
+                   .permitAll()
+                   .anyRequest()
+                   .hasAnyAuthority("ROLE_ADMIN", "ROLE_USER")
+                   .and();
 
 
         // Set unauthorized and forbidden requests exception handler
@@ -74,7 +80,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                    .and();
 
         // Login and logout
-        /*http.formLogin()
+        //http.formLogin().loginPage("/login").loginProcessingUrl("/login");
+       /* http.formLogin()
             .loginProcessingUrl("/login")
             .successHandler((req, resp, auth) -> resp.setStatus(SC_OK)) // success authentication
             .failureHandler((req, resp, ex) -> resp.setStatus(SC_FORBIDDEN))
@@ -84,119 +91,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .invalidateHttpSession(true)
             .deleteCookies("JSESSIONID");*/
 
-        /*
-                .formLogin()
-            .loginPage("/login")
-            *//*.loginProcessingUrl("/login")*//*.and()
-            .exceptionHandling()
-            .authenticationEntryPoint(jwtAuthenticationEntryPoint)
-            *//*.successHandler((req, resp, auth) -> resp.setStatus(SC_OK)) // success authentication
-            .failureHandler((req, resp, ex) -> resp.setStatus(SC_FORBIDDEN))*//*
-         *//*.defaultSuccessUrl("/homepage.html", true)
-            .and()
-            .logout()
-            .logoutUrl("/logout")
-            .deleteCookies("JSESSIONID")
-            .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler())
-            .and()*//*.and()
-            .sessionManagement()
-            .sessionCreationPolicy(SessionCreationPolicy.STATELESS);*/
-
         // Add JWT token filter
         http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
     }
-
-    /*@Override
-    protected void configure(final HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/admin/**").hasRole("ADMIN")
-                .antMatchers("/anonymous*").anonymous()
-                .antMatchers("/login*").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/login.html")
-                .loginProcessingUrl("/perform_login")
-                .defaultSuccessUrl("/homepage.html", true)
-                .failureUrl("/login.html?error=true")
-                .failureHandler(authenticationFailureHandler())
-                .and()
-                .logout()
-                .logoutUrl("/perform_logout")
-                .deleteCookies("JSESSIONID")
-                .logoutSuccessHandler(logoutSuccessHandler());
-
-
-
-
-                 //.authenticationEntryPoint((req, resp, ex) -> resp.setStatus(SC_UNAUTHORIZED));
-    }*/
-
-    /*@Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests()
-            .anyRequest()
-            .hasAnyRole("ADMIN", "USER")
-            .and()
-            .exceptionHandling()
-            .accessDeniedHandler((req, resp, ex) -> resp.setStatus(SC_FORBIDDEN)) // if someone tries to access protected resource but doesn't have enough permissions
-            .authenticationEntryPoint((req, resp, ex) -> resp.setStatus(SC_UNAUTHORIZED))
-            .and()
-            .formLogin()
-            .loginPage("")
-            .loginProcessingUrl("/login")
-            .successHandler((req, resp, auth) -> resp.setStatus(SC_OK)) // success authentication
-            .failureHandler((req, resp, ex) -> resp.setStatus(SC_FORBIDDEN))
-            .and() // bad credentials
-            .sessionManagement()
-            .invalidSessionStrategy((req, resp) -> resp.setStatus(SC_UNAUTHORIZED))
-            .and()
-            .logout()
-            .logoutUrl("/logout")
-            .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler())
-            .and()
-            .csrf()
-            .disable();
-    }*/
-
-
-    // Set permissions on endpoints
-        /*http.authorizeRequests()
-            // Our public endpoints
-            .antMatchers("/authenticate").permitAll()
-            *//*.antMatchers(HttpMethod.GET, "/api/author/**").permitAll()
-            .antMatchers(HttpMethod.POST, "/api/author/search").permitAll()
-            .antMatchers(HttpMethod.GET, "/api/book/**").permitAll()
-            .antMatchers(HttpMethod.POST, "/api/book/search").permitAll()*//*
-            .antMatchers("/users/*").hasRole("ADMIN")
-            // Our private endpoints
-            .anyRequest().authenticated();*/
-
-        /*http.authorizeRequests()
-            .antMatchers("/authenticate")
-            .permitAll() // do not authenticate this particular request
-            .anyRequest()
-            .authenticated()
-            .and() // all other requests need to be authenticated
-            .formLogin()
-            .loginPage("/login")
-            *//*.loginProcessingUrl("/login")*//*.and()
-            .exceptionHandling()
-            .authenticationEntryPoint(jwtAuthenticationEntryPoint)
-            *//*.successHandler((req, resp, auth) -> resp.setStatus(SC_OK)) // success authentication
-            .failureHandler((req, resp, ex) -> resp.setStatus(SC_FORBIDDEN))*//*
-     *//*.defaultSuccessUrl("/homepage.html", true)
-            .and()
-            .logout()
-            .logoutUrl("/logout")
-            .deleteCookies("JSESSIONID")
-            .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler())
-            .and()*//*.and()
-            .sessionManagement()
-            .sessionCreationPolicy(SessionCreationPolicy.STATELESS);*/
-
-    // Add a filter to validate the tokens with every request
-
 }
